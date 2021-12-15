121efa5adeae1314c5bf57c6e64b4b45c08fa6e2
Hermes Alves <hermes@softagon.com.br>
2021-12-15 11:41:15 -0300

    Try to use primeblocks

---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div>
+      <img alt="Vue logo" src="../assets/primeblocks-bug.png" />
+
+      <div>
+        <div class="p-7">
+          <NuxtLink to="/modelo/"> PrimeBlocks bug with Nuxt v3 </NuxtLink>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { useToast } from "primevue/usetoast";
+
+const text = ref();
+const toast = useToast();
+const greet = () => {
+  toast.add({ severity: "info", summary: "Hello " + text.value });
+};
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 1rem;
+  }
+}
+</style>
